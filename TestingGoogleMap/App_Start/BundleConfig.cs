4f4c4ba4a297c45880aee75ced67db7b76f35b31
Pranay Maharana <pranay.maharana@gmail.com>
2017-05-15 13:11:31 -0400

    --map load button

--- TestingGoogleMap/App_Start/BundleConfig.cs
+++ TestingGoogleMap/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace TestingGoogleMap
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js" 
+                        "~/Scripts/jquery-{version}.js", 
+                        "~/Scripts/map.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
--- TestingGoogleMap/Scripts/map.js
+++ TestingGoogleMap/Scripts/map.js
@@ -1,18 +1,15 @@
-﻿(function (window, google) {
-
-    //map options
-    var options = {
-        center: {
-            lat: 27.761767,
-            lng: -82.650683
-        },
-        zoom: 10
-    },
-        element = document.getElementById('map-canvas'),
-        //maop
-        map = new google.maps.Map(element, options);
-
-}(window, google));
+﻿
+function initMap() {
+    var uluru = { lat: 27.761767, lng: -82.650683 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
 
 
 
